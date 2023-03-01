7aeb127c79cd1574ad73ace8798c52d69cd6b102
Hoàng Quân <lehoangquanit@gmail.com>
2023-03-01 21:21:41 +0700

    test head

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,19 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-
+      <Head>
+        <title>Cool Title</title>
+        <meta name="description" content="Checkout our cool page" key="desc" />
+        <meta property="og:title" content="Social Title for Cool Page" />
+        <meta
+          property="og:description"
+          content="And a social description for our cool page"
+        />
+        <meta
+          property="og:image"
+          content="https://example.com/images/cool-page.jpg"
+        />
+      </Head>
     </>
   )
 }
